a529820df21e6e82240a72b991f8fd97247b9674
lucasolr <83046557+lucasolr@users.noreply.github.com>
2024-08-13 10:49:17 -0300

    ADD
    Workflow

---
+++ automacao.py
@@ -0,0 +1,19 @@
+import pyautogui as pa
+import time
+import pyperclip
+pa.PAUSE = 1
+
+pa.press('win')
+pa.write("chrome")
+pa.press('ENTER')
+pa.write( "https://vitoria.dealernetworkflow.com.br/LoginAux.aspx?")
+pa.press('ENTER')
+time.sleep(3)
+pa.click(x=968, y=516)
+pyperclip.copy("LUCAS.OLIMPIO")
+pa.hotkey('ctrl','v')
+pa.click(x=987, y=565)
+pyperclip.copy("LUCAS.2024")
+pa.hotkey('ctrl', 'v')
+pa.click(x=1177, y=640)
+pa.press('ENTER')
